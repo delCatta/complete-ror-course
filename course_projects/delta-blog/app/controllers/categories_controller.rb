92e077a27b0c980b2da4b3f5327fa81b7dbc754d
Diego Cattarinich Clavel <dev.cattarinich@gmail.com>
2022-01-01 14:48:33 -0300

    listing categories test and views

--- course_projects/delta-blog/app/controllers/categories_controller.rb
+++ course_projects/delta-blog/app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
     before_action :set_category, only: [:show]
+    
     def index
+      @categories = Category.paginate(page: params[:page], per_page: 5)
     end
     def new
         @category= Category.new

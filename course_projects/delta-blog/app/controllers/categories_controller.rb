4b8793ba97104d37d252e67faa2c2dad81a426e0
Diego Cattarinich Clavel <dev.cattarinich@gmail.com>
2022-01-01 14:36:15 -0300

    categories tests

--- course_projects/delta-blog/app/controllers/categories_controller.rb
+++ course_projects/delta-blog/app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show]
     def index
     end
     def new
@@ -15,6 +16,10 @@ class CategoriesController < ApplicationController
     end
     end
     private 
+    def set_category
+        @category = Category.find(params[:id])
+      end
+    
     def category_params
         params.require(:category).permit(:name)
       end
